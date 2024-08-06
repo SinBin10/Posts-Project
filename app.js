4f1ac028d578bc8139b286c7c7dfdd68a2595535
SinBin10 <bincode10@gmail.com>
2024-08-07 01:27:54 +0530

    initialized tailwind css

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use("/", (req, res) => {
-  res.send("Feeling good the interview went good..");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(3000);
