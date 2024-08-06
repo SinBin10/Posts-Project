a34ab55c859bdd2d35c7caa4ab54050d268069b4
SinBin10 <bincode10@gmail.com>
2024-08-07 01:46:39 +0530

    created homepage

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("Home");
 });
 
 app.listen(3000);
