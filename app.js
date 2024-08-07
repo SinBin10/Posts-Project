2f199f19510688f2132cc3f2657c503379660b6e
SinBin10 <bincode10@gmail.com>
2024-08-07 18:29:23 +0530

    hashing password in the database.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const userSchema = require("./models/user");
+const bcrypt = require("bcrypt");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -20,16 +21,19 @@ app.get("/post/:userId", (req, res) => {
   res.send("account created succesfully");
 });
 
-app.post("/createuser", async (req, res) => {
-  console.log(req.body);
+app.post("/createuser", (req, res) => {
   let { name, email, password, age } = req.body;
-  let user = await userSchema.create({
-    username: name,
-    email,
-    password,
-    age,
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userSchema.create({
+        username: name,
+        email,
+        password: hash,
+        age,
+      });
+      res.redirect(`/post/${user._id}`);
+    });
   });
-  res.redirect(`/post/${user._id}`);
 });
 
 app.listen(3000);
