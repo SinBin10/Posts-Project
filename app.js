4e76a611067f56cf7debd9cbb189997cc135e447
SinBin10 <bincode10@gmail.com>
2024-08-07 08:19:59 +0530

    creating a user

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const userSchema = require("./models/user");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -9,6 +10,26 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("Home");
+});
+
+app.get("/create", (req, res) => {
+  res.render("create");
+});
+
+app.get("/post/:userId", (req, res) => {
+  res.send("account created succesfully");
+});
+
+app.post("/createuser", async (req, res) => {
+  console.log(req.body);
+  let { name, email, password, age } = req.body;
+  let user = await userSchema.create({
+    username: name,
+    email,
+    password,
+    age,
+  });
+  res.redirect(`/post/${user._id}`);
 });
 
 app.listen(3000);
